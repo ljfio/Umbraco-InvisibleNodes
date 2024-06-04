836917080158ecb00ae1d253be7a801cab429a4a
Luke Fisher <mail@lukefisher.me>
2024-06-04 23:20:25 +0100

    Update integration tests

--- src/Our.Umbraco.InvisibleNodes.Tests.Integration/StandardTests.cs
+++ src/Our.Umbraco.InvisibleNodes.Tests.Integration/StandardTests.cs
@@ -7,31 +7,64 @@ using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using Our.Umbraco.InvisibleNodes.Tests.Integration.Core;
 using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.PublishedCache;
+using Umbraco.Cms.Core.Routing;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Web;
 using Umbraco.Cms.Web.Common.PublishedModels;
 
 namespace Our.Umbraco.InvisibleNodes.Tests.Integration;
 
-public class StandardTests : IDisposable
+public class StandardTests : IClassFixture<TestWebApplicationFactory>, IDisposable
 {
-    private readonly TestWebApplicationFactory _factory = new();
+    private readonly TestWebApplicationFactory _factory;
+
+    public StandardTests(TestWebApplicationFactory factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task Should_Return_Root()
+    {
+        var contextFactory = _factory.Services.GetRequiredService<IUmbracoContextFactory>();
+        using var context = contextFactory.EnsureUmbracoContext();
+
+        var contentService = _factory.Services.GetRequiredService<IContentService>();
+
+        // Content
+        var homeNode = contentService.Create("Home", Constants.System.Root, HomePage.ModelTypeAlias);
+        var homePublishResult = contentService.SaveAndPublish(homeNode);
+
+        homePublishResult.Success.Should().BeTrue();
+
+        // Check pages
+        using var client = _factory.CreateClient();
+
+        var httpResponse = await client.GetAsync("/");
+        httpResponse.EnsureSuccessStatusCode();
+
+        var htmlContent = await httpResponse.Content.ReadAsStringAsync();
+
+        htmlContent.Should().Contain("Home Page: Home");
+    }
 
     [Fact]
     public async Task Should_Return_Child()
     {
         var contextFactory = _factory.Services.GetRequiredService<IUmbracoContextFactory>();
         using var context = contextFactory.EnsureUmbracoContext();
-        
+
         var contentService = _factory.Services.GetRequiredService<IContentService>();
-        
+
         // Content
         var homeNode = contentService.Create("Home", Constants.System.Root, HomePage.ModelTypeAlias);
         var homePublishResult = contentService.SaveAndPublish(homeNode);
 
         homePublishResult.Success.Should().BeTrue();
-        
-        var contentNode = contentService.Create("Content", homeNode, ContentPage.ModelTypeAlias);
+
+        var contentNode = contentService.Create("Content 1", homeNode, ContentPage.ModelTypeAlias);
         var contentPublishResult = contentService.SaveAndPublish(contentNode);
 
         contentPublishResult.Success.Should().BeTrue();
@@ -39,13 +72,89 @@ public class StandardTests : IDisposable
         // Check pages
         using var client = _factory.CreateClient();
 
-        var contentResponse = await client.GetAsync("/content");
-        contentResponse.EnsureSuccessStatusCode();
+        var httpResponse = await client.GetAsync("/content-1");
+        httpResponse.EnsureSuccessStatusCode();
 
-        var content = await contentResponse.Content.ReadAsStringAsync();
+        var htmlContent = await httpResponse.Content.ReadAsStringAsync();
 
-        content.Should().Contain("Content Page: Content");
+        htmlContent.Should().Contain("Content Page: Content 1");
     }
 
-    public void Dispose() => _factory.Dispose();
+    [Fact]
+    public async Task Should_Return_Nested()
+    {
+        var contextFactory = _factory.Services.GetRequiredService<IUmbracoContextFactory>();
+        using var context = contextFactory.EnsureUmbracoContext();
+
+        var contentService = _factory.Services.GetRequiredService<IContentService>();
+
+        // Content
+        var homeNode = contentService.Create("Home", Constants.System.Root, HomePage.ModelTypeAlias);
+        var homePublishResult = contentService.SaveAndPublish(homeNode);
+
+        homePublishResult.Success.Should().BeTrue();
+
+        var contentNode = contentService.Create("Content 2", homeNode, ContentPage.ModelTypeAlias);
+        var contentPublishResult = contentService.SaveAndPublish(contentNode);
+
+        contentPublishResult.Success.Should().BeTrue();
+
+        var nestedNode = contentService.Create("Nested", contentNode, ContentPage.ModelTypeAlias);
+        var nestedPublishResult = contentService.SaveAndPublish(nestedNode);
+
+        nestedPublishResult.Success.Should().BeTrue();
+
+        // Check pages
+        using var client = _factory.CreateClient();
+
+        var httpResponse = await client.GetAsync("/content-2/nested");
+        httpResponse.EnsureSuccessStatusCode();
+
+        var htmlContent = await httpResponse.Content.ReadAsStringAsync();
+
+        htmlContent.Should().Contain("Content Page: Nested");
+    }
+
+    [Fact]
+    public async Task Should_Return_Nested_Same_Name()
+    {
+        var contextFactory = _factory.Services.GetRequiredService<IUmbracoContextFactory>();
+        using var context = contextFactory.EnsureUmbracoContext();
+
+        var contentService = _factory.Services.GetRequiredService<IContentService>();
+
+        // Content
+        var homeNode = contentService.Create("Home", Constants.System.Root, HomePage.ModelTypeAlias);
+        var homePublishResult = contentService.SaveAndPublish(homeNode);
+
+        homePublishResult.Success.Should().BeTrue();
+
+        var contentNode = contentService.Create("Nested", homeNode, ContentPage.ModelTypeAlias);
+        var contentPublishResult = contentService.SaveAndPublish(contentNode);
+
+        contentPublishResult.Success.Should().BeTrue();
+
+        var nestedNode = contentService.Create("Nested", contentNode, ContentPage.ModelTypeAlias);
+        var nestedPublishResult = contentService.SaveAndPublish(nestedNode);
+
+        nestedPublishResult.Success.Should().BeTrue();
+
+        // Check pages
+        using var client = _factory.CreateClient();
+
+        var httpResponse = await client.GetAsync("/nested/nested");
+        httpResponse.EnsureSuccessStatusCode();
+
+        var htmlContent = await httpResponse.Content.ReadAsStringAsync();
+
+        htmlContent.Should().Contain("Content Page: Nested");
+    }
+
+    public void Dispose()
+    {
+        var contentService = _factory.Services.GetRequiredService<IContentService>();
+
+        foreach (var content in contentService.GetRootContent())
+            contentService.Delete(content);
+    }
 }
